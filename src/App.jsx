7e0548f3075452239541bb20048a5900c9e022f0
balo-max <shoni6124@gmail.com>
2025-03-16 09:01:16 +0100

    add huk useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import PropagateLoader from "react-spinners/PropagateLoader";
-import Modal from 'react-modal';
 import toast, { Toaster } from 'react-hot-toast';
+import Modal from 'react-modal';
 
 import './App.css'
 
@@ -12,36 +12,12 @@ import ErrorMessage from './components/ErrorMessage/ErrorMessage';
 import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
 import ImageModal from './components/ImageModal/ImageModal';
 
-
-const customStylesModal = {
-  overlay: { backgroundColor: "rgba(0, 0, 0, 0.9)" },
-  content: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: "90vw",
-    maxWidth: "1200px",
-    height: "90vh",
-    maxHeight: "1000px",
-    border: "unset",
-    background: "transparent",
-    padding: "0",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    overflow: "hidden",   
-  }
-};
-
-let currentPage = 1;
-
 Modal.setAppElement("#root");
 
 function App() {
   const [images, setImages] = useState([]);
   const [query, setQuery] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
   const [isLoader, setIsLoader] = useState(false);
   const [isError, setIsError] = useState(false);
   const [isLoadMore, setIsLoadMore] = useState(false);
@@ -49,90 +25,85 @@ function App() {
   const [selectedImage, setSelectedImage] = useState(null);
 
   const handledRequestImage = async (value) => {
-    try {
-      setIsLoadMore(false);
-      setIsLoader(true);
-      setImages([]);
-      setQuery(value);
-
-      currentPage = 1;
-
-      const response = await getImages(value, currentPage);
-      const data = response.results
-      const totalPages = response.total_pages
-
-      setImages(data);
-      setIsLoader(false);
-      if (totalPages >= 1) { return setIsLoadMore(true) };
-    }
-    catch (er) {
-      console.log(er);
-      setIsError(er);
-    }
-    finally {
-      setIsLoader(false);
-    }
+    setImages([]);
+    setIsError(false);
+    setIsLoadMore(false);
+    setIsLoader(true);
+    setQuery(value);
+    setCurrentPage(1)
   };
 
-  const handledLoadMoreImages = async () => {
-    try {
-      setIsLoadMore(false);
-      setIsLoader(true);
+  const handledLoadMoreImages = () => {
+    setIsLoadMore(false);
+    setIsLoader(true);
+    setCurrentPage(currentPage + 1);
+    console.log(currentPage);
+  };
 
-      currentPage += 1;
+  useEffect(() => {
+    if (!query) return;
+    async function fetchImages() {
+      try {
+        const response = await getImages(query, currentPage);
+        const data = response.results
+        const totalPages = response.total_pages
+        if (totalPages === 0) {
+          setIsLoader(false);
+          setIsLoadMore(false);
+          return toast.error('No results!');
+        };
 
-      const response = await getImages(query, currentPage);
-      const data = response.results
-      const totalPages = response.total_pages
+        setImages(prevImages => [...prevImages, ...data]);
 
-      setImages(prevImages => [...prevImages, ...data]);
-
-      setTimeout(() => {
-      window.scrollBy({
-        top: 800,
-        behavior: "smooth"
-      });
-      }, 300);
+        setTimeout(() => {
+          window.scrollBy({
+            top: 800,
+            behavior: "smooth"
+          });
+        }, 300);
       
-      setIsLoader(false);
-      setIsLoadMore(true);
-      if (totalPages === currentPage) {
-        toast.error('No more images!',
-          {
-            style: {
-              width: '200px',
-              fontSize: '20px'
+        setIsLoader(false);
+        if (totalPages >= 1) { return setIsLoadMore(true) };
+        setIsLoadMore(true);
+        if (totalPages === currentPage) {
+          toast.error('No more images!',
+            {
+              style: {
+                width: '200px',
+                fontSize: '20px'
+              }
             }
-          }
-        );
-        return setIsLoadMore(false);
-      };
-    }
-    catch (er) {
-      console.log(er);
-      setIsError(er);
-    }
-    finally {
-      setIsLoader(false);
-    }
-  };
+          );
+          return setIsLoadMore(false);
+        };
+      }
+      catch (er) {
+        console.log(er);
+        setIsError(true);
+      }
+      finally {
+        setIsLoader(false);
+      }
+    };
+    fetchImages();
+  }, [query, currentPage])
+
 
   const handledOpenModal = (image) => {
     setSelectedImage(image)
     setModalIsOpen(true);
   };
 
+  const handledCloseModal = () => {
+    setModalIsOpen(false);
+  }
+
 
   return (
     <>
       <SearchBar onSubmit={handledRequestImage} />
       {isError ? <ErrorMessage /> : <ImageGallary images={images} onOpenModal={handledOpenModal} />}
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={() => setModalIsOpen(false)}
-        style={customStylesModal}>
-        <ImageModal image={selectedImage} />
-      </Modal>
+      <ImageModal isOpen={modalIsOpen} onCloseModal={handledCloseModal} image={selectedImage} />
       {isLoadMore && <LoadMoreBtn onSubmit={handledLoadMoreImages} />}
       {isLoader && <PropagateLoader color='#741414' />}
       <Toaster
--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,11 +1,44 @@
+import Modal from 'react-modal';
+
 import css from './ImageModal.module.css'
 
-export default function ImageModal({image}) {
+const customStylesModal = {
+  overlay: { backgroundColor: "rgba(0, 0, 0, 0.9)" },
+  content: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "fit-content",
+    height: "90vh",
+    border: "unset",
+    background: "transparent",
+    padding: "0",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",   
+  }
+};
+
+
+
+export default function ImageModal({ image, onCloseModal, isOpen }) {
+    if (!image) return;
     return (
-        <div className={css.wrapperModal}>
-            <img className={css.modalImage} src={image.urls.regular} alt={image.description} />
-            <p className={css.modalText}>Author: {image.user.name}</p>
-            <p className={css.modalText}>Location: {image.user.location}</p>
-        </div>
+        <Modal
+            isOpen={isOpen}
+            style={customStylesModal}
+            onRequestClose={onCloseModal}
+        >
+
+            <div className={css.wrapperModal}>
+                <img className={css.modalImage} src={image.urls.regular} alt={image.description} />
+                <p className={css.modalText}>Author: {image.user.name}</p>
+                <p className={css.modalText}>Location: {image.user.location}</p>
+            </div>
+        </Modal>
     );
-}
+};
+    
